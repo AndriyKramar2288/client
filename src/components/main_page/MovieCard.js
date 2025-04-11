15a945854d7d4b8f36e0e78a507129669d946558
Andriyko <andriykramar465@gmail.com>
2025-04-11 20:57:18 +0300

    feat: finished development of the main page design
    Finished working on the MovieCard.js design (view)

--- src/components/main_page/MovieCard.js
+++ src/components/main_page/MovieCard.js
@@ -5,9 +5,30 @@ import { useEffect, useState } from "react"
 
 const PHOTOS_INTERVAL_VALUE = 5000
 
-const ACTORS_H1 = "Актори"
+const LOOK_FOR_WATCHS_TEXT = "Переглянути сенси"
 const DESCRIPTION_H1 = "Опис"
 const IMDB_SVG_SRC = "https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+
+const DETAILS_LIST_ITEM_TEXTS = {
+    director: { text: "Режисер", icon: "fa-solid fa-user-tie" },
+    duration: { text: "Тривалість", icon: "fa-solid fa-film" },
+    countries: { text: "Країни", icon: "fa-solid fa-font-awesome" },
+    voice_acting: { text: "Дубляж", icon: "fa-solid fa-quote-right" },
+    actors: { text: "Актори", icon: "fa-solid fa-quote-right" }
+};
+
+
+function MovieCardButton({ clickHandler, text }) {
+    return (
+        <div className="group flex m-4">
+            <div className="w-0 group-hover:pr-2 duration-300 bg-amber-700" />
+            <button onClick={clickHandler} style={{ fontFamily: "var(--font-pt-mono)" }} className="cursor-pointer bg-amber-700 hover:bg-amber-800 duration-300 px-3 group-hover:px-1 py-2 whitespace-nowrap">
+                {text}
+            </button>
+            <div className="w-0 group-hover:pl-2 duration-300 bg-amber-700" />
+        </div>
+    )
+}
 
 function DetailsListItem({first, last, icon_class}) {
     return (
@@ -28,32 +49,48 @@ function FilmDetails({ film, currentPhoto }) {
             exit={{ opacity: 0, x: 100, transition: { duration: 0.25 } }}
             style={{ fontFamily: "var(--font-pt-mono)", backgroundImage: `url(${film.src_photos[currentPhoto]})` }}>
 
-            <motion.div className="backdrop-sepia-50 h-full flex p-4 rounded-2xl"
+            <motion.div className="backdrop-sepia-50 h-full flex p-2 rounded-2xl"
                         key={currentPhoto}
                         initial={{ backdropFilter: "blur(100px)" }}
                         animate={{ backdropFilter: "blur(4px)", transition: { duration: 0.7, ease: "easeOut" } }}
                         exit={{ backdropFilter: "blur(100px)", transition: { duration: 0.1, ease: "easeIn" } }}>
-                <div className="flex flex-col p-3">
+                <div className="flex flex-col p-1 max-w-[50vw]">
                     <ul className="bg-[#00000088] p-5 rounded-lg">
-                        <DetailsListItem first={"Режисер"} last={film.director} icon_class={"fa-solid fa-user-tie"} />
-                        <DetailsListItem first={"Тривалість"} last={`${film.duration} хв`} icon_class={"fa-solid fa-film"} />
-                        <DetailsListItem first={"Країни"} last={film.countries.join(", ")} icon_class={"fa-solid fa-font-awesome"} />
-                        <DetailsListItem first={"Дубляж"} last={film.voice_acting} icon_class={"fa-solid fa-quote-right"} />
+                        <DetailsListItem 
+                            first={DETAILS_LIST_ITEM_TEXTS["director"].text} 
+                            last={film.director} 
+                            icon_class={DETAILS_LIST_ITEM_TEXTS["director"].icon} 
+                            />
+
+                        <DetailsListItem 
+                            first={DETAILS_LIST_ITEM_TEXTS["duration"].text} 
+                            last={`${film.duration} хв`} 
+                            icon_class={DETAILS_LIST_ITEM_TEXTS["duration"].icon} 
+                            />
+
+                        <DetailsListItem 
+                            first={DETAILS_LIST_ITEM_TEXTS["countries"].text} 
+                            last={film.countries.join(", ")} 
+                            icon_class={DETAILS_LIST_ITEM_TEXTS["countries"].icon} 
+                            />
+
+                        <DetailsListItem 
+                            first={DETAILS_LIST_ITEM_TEXTS["voice_acting"].text} 
+                            last={film.voice_acting} 
+                            icon_class={DETAILS_LIST_ITEM_TEXTS["voice_acting"].icon} 
+                            />
+
+                        <DetailsListItem 
+                            first={DETAILS_LIST_ITEM_TEXTS["actors"].text} 
+                            last={film.actors.join(", ")} 
+                            icon_class={DETAILS_LIST_ITEM_TEXTS["actors"].icon} 
+                            />
                     </ul>
 
-                    <h1 className="mt-4 font-bold text-center">{ACTORS_H1}</h1>
-                    <ul className="bg-[#00000088] p-3 rounded-lg mt-2 text-sm">
-                        {film.actors.map((actor, index) => (
-                            <li className="mt-1" key={index}>
-                                <i className="fa-regular fa-circle mr-2 text-xs"></i>
-                                {actor}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div>
-                    <h1 className="mt-4 font-bold text-center">{DESCRIPTION_H1}</h1>
-                    <p className="max-w-[30vw] text-xs bg-[#00000088] p-5 rounded-lg mt-3">{film.about}</p>
+                    <div>
+                        <h1 className="mt-6 font-bold text-center">{DESCRIPTION_H1}</h1>
+                        <p className="text-sm bg-[#000000cf] p-5 rounded-lg mt-2">{film.about}</p>
+                    </div>
                 </div>
             </motion.div>
         </motion.div>
@@ -94,7 +131,7 @@ export default function MovieCard({ film, hide_details_other_card, extanded, vis
                 initial={{ opacity: 0, width: 0 }}
                 animate={{ opacity: 1, width: extanded ? "100%" : "50%", transition: { duration: 0.7 } }}
                 exit={{ opacity: 0, width: 0, transition: { duration: 0.7 } }}
-                className="flex text-amber-100 my-4 overflow-hidden h-[75lvh]"
+                className="flex text-amber-100 my-4 overflow-hidden items-start"
                 key={card_index}>
                 <div onClick={(e) => toggleExtanded()} className="bg-[#585858d3] rounded-tl-2xl flex justify-center items-center cursor-pointer duration-300 hover:bg-amber-700">
                     <motion.img
@@ -102,7 +139,7 @@ export default function MovieCard({ film, hide_details_other_card, extanded, vis
                         animate={{ opacity: 1, transition: { duration: 0.5, delay: 0.15 } }}
                         exit={{ opacity: 0, transition: { duration: 0.5 } }}
                         key={film.src_poster}
-                        src={film.src_poster} alt="poster" className="shadow-xl shadow-black max-h-[70lvh] rounded-md mx-2.5" />
+                        src={film.src_poster} alt="poster" className="shadow-xl shadow-black max-h-[70lvh] rounded-md m-2.5 contain-content" />
                 </div>
                 <div style={{ fontFamily: "var(--font-cormorant)" }} className="flex flex-col bg-[#38383862] rounded-br-2xl">
                     <h1 className="m-3 text-3xl">{film.uk_name}</h1>
@@ -114,8 +151,9 @@ export default function MovieCard({ film, hide_details_other_card, extanded, vis
                     <h1 className="text-xl mx-3 my-0.5 text-amber-200">{film.release_year}</h1>
                     <div className="flex items-center mx-3">
                         <img src={IMDB_SVG_SRC} className="max-h-5" />
-                        <h1 className="text-xl mx-3 my-1 text-amber-200">{film.rating.imdb}</h1>
+                        <h1 className="text-xl m-3 text-amber-200">{film.rating.imdb}</h1>
                     </div>
+                    <MovieCardButton text={LOOK_FOR_WATCHS_TEXT} />
                 </div>
                 {extanded && (
                     <FilmDetails currentPhoto={currentPhoto} film={film} />
